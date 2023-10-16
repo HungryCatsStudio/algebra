b0114dd0d5046f052688e054041efbc158cbe45c
Antonio Mejías Gil <anmegi.95@gmail.com>
2023-10-16 11:04:24 +0200

    changed to IntoIter

--- poly/src/evaluations/multivariate/multilinear/mod.rs
+++ poly/src/evaluations/multivariate/multilinear/mod.rs
@@ -1,6 +1,7 @@
 mod dense;
 mod sparse;
 
+use alloc::vec::IntoIter;
 pub use dense::DenseMultilinearExtension;
 pub use sparse::SparseMultilinearExtension;
 
@@ -41,7 +42,7 @@ pub trait MultilinearExtension<F: Field>:
     + for<'a> AddAssign<(F, &'a Self)>
     + for<'a> SubAssign<&'a Self>
     + Index<usize>
-    + Polynomial<F> where Self::Point: IntoIterator<Item = F>
+    + Polynomial<F> where Self::Point: IntoIter<Item = F>
 {
     /// Returns the number of variables in `self`
     fn num_vars(&self) -> usize;
