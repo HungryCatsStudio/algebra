6e671acba5d192d92d0a99cadb9d8ba1f4c11777
Antonio Mejías Gil <anmegi.95@gmail.com>
2023-11-14 11:46:35 +0100

    made small size public

--- ec/src/scalar_mul/variable_base/mod.rs
+++ ec/src/scalar_mul/variable_base/mod.rs
@@ -9,7 +9,7 @@ pub use stream_pippenger::*;
 
 use super::ScalarMul;
 
-const SMALLNESS: usize = 60;
+pub const SMALLNESS: usize = 60;
 
 pub trait VariableBaseMSM: ScalarMul {
     /// Computes an inner product between the [`PrimeField`] elements in `scalars`
